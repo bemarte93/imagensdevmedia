c9e6ce576b106d85033d22fc7d840b1f779acf80
bemarte93 <44785129+bemarte93@users.noreply.github.com>
2018-11-05 16:22:01 -0300

    Add files via upload

---
+++ conecta.php
@@ -0,0 +1,12 @@
+<?php
+  $conexao = mysql_connect("den1.mysql4.gear.host", "imagensdevmedia", "Wa036S~F40r-");
+    
+  if($conexao)
+  {
+  $baseSelecionada = mysql_select_db("imagensdevmedia");
+  if (!$baseSelecionada) {
+      die ('Não foi possível conectar a base de dados: ' . mysql_error());
+  } } else {
+      die('Não conectado : ' . mysql_error());
+  }
+  ?>
