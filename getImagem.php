6ea1bac90bc8b68663165d4335d2e3133cfb0ce0
bemarte93 <44785129+bemarte93@users.noreply.github.com>
2018-11-05 16:55:30 -0300

    Update getImagem.php

--- getImagem.php
+++ getImagem.php
@@ -1,8 +1,9 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "test";
+    require("conecta.php");
+    $host = "den1.mysql4.gear.host";
+    $username = "imagensdevmedia";
+    $password = "Wa036S~F40r-";
+    $db = "imagensdevmedia";
     $PicNum = $_GET["PicNum"];
  
     mysql_connect($host,$username,$password) or die("Impossível conectar ao banco."); 
