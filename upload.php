1eaf7e3cc0353e38b036606108746b8746579cf4
bemarte93 <44785129+bemarte93@users.noreply.github.com>
2018-11-05 16:48:38 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,30 +1,31 @@
 <?php
-  require("conecta.php");
-  $nomeEvento = $_POST['nome_evento'];
-  $descricaoEvento = $_POST['descricao_evento'];
-  $imagem = $_FILES['imagem']['tmp_name'];
-  $tamanho = $_FILES['imagem']['size'];
-  $tipo = $_FILES['imagem']['type'];
-  $nome = $_FILES['imagem']['name'];
-    
-  if ( $imagem != "none" )
-  {
-      $fp = fopen($imagem, "rb");
-      $conteudo = fread($fp, $tamanho);
-      $conteudo = addslashes($conteudo);
-      fclose($fp);
-    
-  $queryInsercao = "INSERT INTO tabela_imagens (evento, descrição, nome_imagem, tamanho_image, tipo_imagem, imagem) VALUES ('$nomeEvento', '$descricaoEvento','$nome','$tamanho', '$tipo','$conteudo')";
-    
-  mysql_query($queryInsercao) or die("Algo deu errado ao inserir o registro. Tente novamente.");
-    
-  echo 'Registro inserido com sucesso!'; 
-  header('Location: index.php');
-   if(mysql_affected_rows($conexao) > 0)
-       print "A imagem foi salva na base de dados.";
-   else
-       print "Não foi possível salvar a imagem na base de dados.";
-   }
-  else
-      print "Não foi possível carregar a imagem.";
-  ?>
+ 
+$imagem = $_FILES["imagem"];
+$host = "den1.mysql4.gear.host";
+$username = "imagensdevmedia";
+$password = "Wa036S~F40r-";
+$db = "imagensdevmedia";
+ 
+if($imagem != NULL) { 
+    $nomeFinal = time().'.jpg';
+    if (move_uploaded_file($imagem['tmp_name'], $nomeFinal)) {
+        $tamanhoImg = filesize($nomeFinal); 
+ 
+        $mysqlImg = addslashes(fread(fopen($nomeFinal, "r"), $tamanhoImg)); 
+ 
+        mysql_connect($host,$username,$password) or die("Impossível Conectar"); 
+ 
+        @mysql_select_db($db) or die("Impossível Conectar"); 
+ 
+        mysql_query("INSERT INTO PESSOA (PES_IMG) VALUES ('$mysqlImg')") or die("O sistema não foi capaz de executar a query"); 
+ 
+        unlink($nomeFinal);
+         
+        header("location:exibir.php");
+    }
+} 
+else { 
+    echo"Você não realizou o upload de forma satisfatória."; 
+} 
+ 
+?>
